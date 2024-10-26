f9a0ba150154301f7764a6251c7af5717244cb7d
triggerwaffle <53524398+triggerwaffle@users.noreply.github.com>
2024-10-26 16:53:29 +0900

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,15 @@
 setInterval(function() {
   document.getElementById("d2").innerHTML = new Date().toLocaleString();
 }, 1000);
+
+
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      }
+    }
+  }
+}

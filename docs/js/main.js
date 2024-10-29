4de48b0306d3d543157693792c56d25f41933ba7
triggerwaffle <53524398+triggerwaffle@users.noreply.github.com>
2024-10-29 20:17:14 +0900

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,6 +2,7 @@ $vui.config.importMap = {
   "*": "./components/${component}.html",
 };
 
-setInterval(function () {
+/*setInterval(function () {
   document.getElementById("d2").innerHTML = new Date().toLocaleString();
 }, 1000);
+*/
